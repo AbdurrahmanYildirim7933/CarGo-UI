408134d575ef923c09194667507e1c815d69d1d6
dilanergin <dilanrgn@gmail.com>
2023-08-07 11:16:10 +0300

    login patch created

--- src/app/login/cookieUtils.ts
+++ src/app/login/cookieUtils.ts
@@ -1,1 +1,13 @@
-import {Component} from "@angular/core";
+
+export class CookieUtils {
+  private isConsented: boolean = false;
+
+
+   setCookie(name: string, value: string, expireDays: number, path: string = '') {
+    let d:Date = new Date();
+    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    let expires:string = `expires=${d.toUTCString()}`;
+    let cpath:string = path ? `; path=${path}` : '';
+    document.cookie = `${name}=${value}; ${expires}${cpath}`;
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {UserDTO} from "./UserDTO";
 import {HttpClient} from "@angular/common/http";
+import {CookieUtils} from "./cookieUtils";
 
 @Component({
   selector: 'app-login',
@@ -12,14 +13,14 @@ email: string;
 password: string;
 apiUrl="http://localhost:8080";
 
-constructor(private http: HttpClient) {
+constructor(private http: HttpClient,private cookie:CookieUtils) {
 }
   login(): void{
     const user: UserDTO = new UserDTO(this.email, this.password);
 
     this.http.post<any>(`${this.apiUrl}/api/v1/auth/login`, user).subscribe(
       (response) => {
-        // Backend'den gelen cevapları burada işleyebilirsiniz
+        this.cookie.setCookie()
         console.log('Kullanıcı girişi başarılı:', response);
       },
       (error) => {
